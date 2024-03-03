f4a7a1f9662b03d73ab66f8cd24d320fe634dbaf
Boubacar Diallo <bd015720@gmail.com>
2024-03-03 01:04:43 +0000

    first commit

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,77 @@
 import { useEffect, useState } from "react"
+import "./App.css"
+import clear from "./images/clear.jpeg"
+import rain from  "./images/rain.jpg"
+import clouds from  "./images/clouds.jpeg"
+import mist from "./images/mist.png"
+
 
 const Weather =  () => {
     const [data, setData] = useState("")
-    const [input, setInput] = useState("")
-    const [inpVal, setInpVal] = useState([])
+    const [input, setInput] = useState("london")
+    const [inpVal, setInpVal] = useState(null)
     
         
-
+    
     const fetchData = async () =>{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=london&appid=e7379634c4453e11eea8065725ba733e&units=metric`)
+        try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=e7379634c4453e11eea8065725ba733e&units=metric`)
         const advice = await response.json()
         setData(advice)
-        console.log(data.main.temp)
+        } catch(error){
+            alert(error)
+        }
+        // console.log(data.weather[0].main)
+        
+    }
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setInpVal(...input)
+        setInput("")    
     }
 
     useEffect(()=>{
         fetchData()
     }, [])
 
+    const weather = {
+        "Clouds":clouds,
+        "Clear":clear,
+        "Rain":rain,
+        "Mist":mist,
+    }
+
 
     return (
-        <div>
-            <h1>Weather App</h1>
+        <div className="weather">
+            <h1 className="title">Weather App</h1>
             <div>
-            <h1>london</h1>
-            <input></input>
-            {/* <span>{data.weather.description}</span> */}
-            <button onClick={fetchData}>Weather</button>
-            <div>
-            <span>Description: {data.weather[0].description}</span>
-            <span>Temperature: {data.main.temp}</span>
-            <span>Max temp: {data.main.temp_max}</span>
-            <span>Min temp: {data.main.temp_min}</span>
-            <span>Humidity: {data.main.humidity}</span>
-            <span>Wind speed: {data.wind.speed}</span>
-            </div>
-            
+            <form onSubmit={handleSubmit}>
+            <input 
+            type="text"
+            value={input}
+            onChange={(e)=>setInput(e.target.value)} 
+            className="input"/>
+            <button className="button" onClick={fetchData}>Weather</button>
+            </form>
+
+            {inpVal && <div className="info">
+            <h2 className="info-subtitle">{data.name} weather: {data.weather[0].main}</h2>
+            <img width="234px"height= "150px" className="info-img" src={weather[data.weather[0].main]}></img>                
+            <span className="info-span">Description: {data.weather[0].description}</span>
+            <span className="info-span">Temperature: {data.main.temp}°C</span>
+            <span className="info-span">Max temperature: {data.main.temp_max}°C</span>
+            <span className="info-span">Min temperature: {data.main.temp_min}°C</span>
+            <span className="info-span">Humidity: {data.main.humidity}%</span>
+            <span className="info-span">Wind speed: {data.wind.speed}mph</span>
+            </div>}            
         </div>
-            
         </div>
     )
+    
 }
+
 
 export default Weather
